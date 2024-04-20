04c35f9472040c3721f2b24beb8119ff657654da
AHMED Shaik <shabbersk3@gmail.com>
2024-04-20 13:57:11 +0530

    added docker files

--- server/src/payments/payments.module.ts
+++ server/src/payments/payments.module.ts
@@ -1,16 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      useFactory: async () => ({
-        uri: 'mongodb://localhost:27017/hellos',
-      }),
-    }),
     PaymentsModule,
     DatabaseModule,
   ],
